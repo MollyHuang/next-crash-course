841100469178c1a771fbe5eb25ca19d3adb5d217
Molly Huang <molly.h@gmail.com>
2021-03-21 19:56:19 +0800

    #2 - 20:56​ - Nav Component & Link

---
+++ components/Nav.js
@@ -0,0 +1,20 @@
+//sfc + tab
+import Link from 'next/link'
+import navStyles from '../styles/Nav.module.css'
+
+const Nav = ({ children }) => {
+  return (
+    <div className={navStyles.nav}>
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Nav;
