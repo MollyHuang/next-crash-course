652cf21893be911b2605c9f02aad1a7c2d9a5a58
Molly Huang <molly.h@gmail.com>
2021-03-21 20:46:10 +0800

    #5 - ArticleList 31:16​ - Data Fetching 32:02​ - getStaticProps() 33:58​ - Showing Data

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,26 @@
 import Head from 'next/head'
+import ArticleList from '../components/ArticleList'
 
-export default function Home() {
+export default function Home({ articles }) {
+  // console.log('articles = ', articles)
   return (
     <>
       <Head>
         <title>Next Crash - Home</title>
         <meta name="keywords" content="Next, React, Web Development" />
       </Head>
-      <div>
-        <h1>Welcome to Next</h1>
-      </div>
+      <ArticleList articles={articles} />
     </>
   )
+}
+
+export const getStaticProps = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`);
+  const articles = await res.json()
+
+  return {
+    props: {
+      articles
+    }
+  }
 }
