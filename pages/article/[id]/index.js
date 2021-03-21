2c860809774dea47c2d1adbe8bae4d61ecec240a
Molly Huang <molly.h@gmail.com>
2021-03-21 21:04:05 +0800

    #7 - 40:15​ - Nested Routing

---
+++ pages/article/[id]/index.js
@@ -0,0 +1,13 @@
+//sfc + tab
+import { useRouter } from 'next/router'
+
+const article = () => {
+  const router = useRouter()
+  const { id } = router.query
+
+  return (
+    <div>This is article {id}</div>
+  );
+}
+
+export default article;
