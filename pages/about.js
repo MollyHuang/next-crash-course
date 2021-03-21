3296570d27aed7a05c30b5535f77b4abb24cdabf
Molly Huang <molly.h@gmail.com>
2021-03-21 19:43:24 +0800

    #1 - Pages & Routing 8:23​ - Files & Folders 11:37​ - Pages & Routing 13:14​ - Head 16:05​ - Layouts & CSS Modules

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,10 @@
 import Head from 'next/head'
 
-export default function Home() {
+export default function About() {
   return (
     <>
       <Head>
-        <title>Next Crash - Home</title>
+        <title>Next Crash - About</title>
         <meta name="keywords" content="Next, React, Web Development" />
       </Head>
       <div>
