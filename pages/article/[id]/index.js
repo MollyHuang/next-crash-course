1b5f7380ee00df5c4f405964d321c650529af528
Molly Huang <molly.h@gmail.com>
2021-03-22 21:03:40 +0800

    #12 - 59:24​ - Using the API Data config/index.js pages/index.js pages/article/[id]/index.js

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,6 +1,7 @@
 //sfc + tab
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import { server } from '../../../config'
 
 const article = ({ article }) => {
   // const router = useRouter()
@@ -17,7 +18,8 @@ const article = ({ article }) => {
 }
 
 export const getStaticProps = async (context) => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`);
+  // const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`);
+  const res = await fetch(`${server}/api/articles/${context.params.id}`);
   const article = await res.json()
 
   return {  // will be passed to the page component as props
@@ -28,7 +30,8 @@ export const getStaticProps = async (context) => {
 }
 
 export const getStaticPaths = async () => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+  // const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+  const res = await fetch(`${server}/api/articles`);
   const articles = await res.json()
 
   const paths = articles.map(article => ({ params: { id: article.id.toString() } }))
